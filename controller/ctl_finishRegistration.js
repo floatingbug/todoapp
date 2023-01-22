973314d509ff111b023507b76ecf6dbc9210df50
floatingbug <sieber.thomas@yahoo.com>
2023-01-22 11:31:52 +0100

    new from notebook

---
+++ controller/ctl_finishRegistration.js
@@ -0,0 +1,24 @@
+const CLIENT_URL = process.env.CLIENT_URL || 'localhost:5173';
+const registration_url = `http://${CLIENT_URL}/#/registration`;
+const login_url = `http://${CLIENT_URL}/#/login`;
+const module_removeConfirmCode = require('../module/module_removeConfirmCode');
+
+module.exports = async (req, res)=> {
+	let code = req.query.reg;
+	
+	const result = await module_removeConfirmCode(code);
+	
+	//db error
+	if(result === 3){
+		res.redirect(registration_url);
+		return 0;
+	}
+
+	//fail to set reg_code to 0
+	if(result.modifiedCount === 0){
+		res.redirect(registration_url);
+		return 0;
+	}
+	
+	res.redirect(login_url);
+};
--- module/module_removeConfirmCode.js
+++ module/module_removeConfirmCode.js
@@ -1,15 +1,25 @@
 const client = require('./create_mongo_client');
 
-module.exports = async (reg_code){
+module.exports = async (reg_code)=>{
+	let result = {};
+
 	try{
 		await client.connect();
 		const db = client.db('todoapp');
 		const collection = db.collection('user');
-		let result = await collection.updateOne({reg_code: req_code}, {
+		result = await collection.updateOne({reg_code: reg_code}, {
 			$set:{
 				reg_code: 0
 			}
 		});
 	}
+	catch(e){
+		console.log("DB-Error in module_removeConfirmCode: ", e);
+		return 3;
+	}
+	finally{
+		client.close();
+	}
 	
+	return result;
 };
