20e81edbbd3b47464b52fe2b20814755b4536340
floatingbug <sieber.thomas@yahoo.com>
2023-01-28 10:13:16 +0100

    new from notebook

--- module/module_removeConfirmCode.js
+++ module/module_removeConfirmCode.js
@@ -2,12 +2,13 @@ const client = require('./create_mongo_client');
 
 module.exports = async (reg_code)=>{
 	let result = {};
+	const code = parseInt(reg_code);
 
 	try{
 		await client.connect();
 		const db = client.db('todoapp');
 		const collection = db.collection('user');
-		result = await collection.updateOne({reg_code: reg_code}, {
+		result = await collection.updateOne({reg_code: code}, {
 			$set:{
 				reg_code: 0
 			}
