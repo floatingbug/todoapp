5d05fb873b3c0e96882e40b100ab61dcfde051ad
floatingbug <sieber.thomas@yahoo.com>
2023-01-30 10:36:46 +0100

    new from notebook

--- vue_app/src/components/login.vue
+++ vue_app/src/components/login.vue
@@ -1,7 +1,16 @@
 <script setup>
-	
+import {ref} from 'vue'
 </script>
 
 <template>
 	<h1>Login</h1>
+	<form v-on:submit.prevent="submitForm">
+		<label for="name">name</label>
+		<label for="password">password</label>
+		<label for="login">login</label>
+		<input type="submit" value="submit">
+	</form>
+
+	<textarea id="txt_area" name="" cols="30" rows="10">{{state.name}}
+		, and {{state.password}}</textarea>
 </template>
