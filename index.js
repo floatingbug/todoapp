583ad92daa1bac39be33c1cafce8bb8bc363e243
tornadohead <sieber.thomas@yahoo.com>
2023-02-15 12:49:30 +0100

    serverless

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoStore = require('connect-mongo');
 const serveStatic = require('serve-static');
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/vue_app/dist'));
 app.use(cors());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: false}));
