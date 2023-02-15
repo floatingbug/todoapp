d4c912f468a4d31c818307373d531de18b5332ba
tornadohead <sieber.thomas@yahoo.com>
2023-02-15 12:48:16 +0100

    serverless

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
-/*
 const body_parser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
 const session = require('express-session');
 const mongoStore = require('connect-mongo');
 const serveStatic = require('serve-static');
-*/
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
-/*
 app.use(cors());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: false}));
@@ -21,5 +18,4 @@ app.use(session({
 	store: mongoStore.create({ mongoUrl: 'mongodb://localhost/todo-session-store' })
 }));
 app.use('/', router);
-*/
 httpServer.listen(PORT);
