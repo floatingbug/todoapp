59385c715a92c29fd1d7d6206fd894fc32a643e4
tornadohead <sieber.thomas@yahoo.com>
2023-02-15 12:30:48 +0100

    serverless

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const serveStatic = require('serve-static');
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/vue_app/dist'));
-
+/*
 app.use(cors());
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended: false}));
@@ -19,5 +19,5 @@ app.use(session({
 	store: mongoStore.create({ mongoUrl: 'mongodb://localhost/todo-session-store' })
 }));
 app.use('/', router);
-
+*/
 httpServer.listen(PORT);
